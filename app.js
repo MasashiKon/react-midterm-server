f211359967d3abacb6e8cae169c1e67fafb6e27b
MasahiKon <konnonorth@gmail.com>
2023-03-24 19:34:26 -0700

    delete cross origin

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const nameList = ["Owl"];
 const express = require("express");
 const app = express();
 const http = require('http');
-const cors = require('cors');
+
 const { Server } = require('socket.io');
-app.use(cors());
+
 
 const PORT = 5000;
 
@@ -13,12 +13,7 @@ const axios = require('axios');
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: "https://git-midterm-client.vercel.app",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 
 io.on("connection", (socket) => {
     console.log(`User connected ${socket.id}`);
